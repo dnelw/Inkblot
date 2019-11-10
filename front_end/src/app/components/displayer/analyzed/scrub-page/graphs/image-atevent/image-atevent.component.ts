976b660b389575f231d46cebd73dd5a67071da44
Daniel Wang <dnelw@outlook.com>
2019-11-10 02:22:52 -0500

    CSS touches

--- front_end/src/app/components/displayer/analyzed/scrub-page/graphs/image-atevent/image-atevent.component.ts
+++ front_end/src/app/components/displayer/analyzed/scrub-page/graphs/image-atevent/image-atevent.component.ts
@@ -1,33 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { Store, select } from '@ngrx/store';
-import { IAppState } from 'src/app/store/state/app.state';
-import { selectIndex, selectData } from 'src/app/store/selectors/data.selectors';
+import { Component, OnInit } from "@angular/core";
+import { Store, select } from "@ngrx/store";
+import { IAppState } from "src/app/store/state/app.state";
+import {
+  selectIndex,
+  selectData
+} from "src/app/store/selectors/data.selectors";
 
 @Component({
-  selector: 'app-image-atevent',
-  templateUrl: './image-atevent.component.html',
-  styleUrls: ['./image-atevent.component.scss']
+  selector: "app-image-atevent",
+  templateUrl: "./image-atevent.component.html",
+  styleUrls: ["./image-atevent.component.scss"]
 })
 export class ImageAteventComponent implements OnInit {
   indexData$ = this.store.pipe(select(selectIndex));
   vidData$ = this.store.pipe(select(selectData));
   i: number;
   image: string;
-  
-  constructor(
-    private store: Store<IAppState>
-  ) { }
+
+  constructor(private store: Store<IAppState>) {}
 
   ngOnInit() {
-
     this.indexData$.subscribe(indexVal => {
       this.i = indexVal;
 
       this.vidData$.subscribe(data => {
         this.image = data[this.i].image;
-      })
-    })
-
+      });
+    });
+    console.log(this.image);
   }
-
 }
